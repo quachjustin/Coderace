540195f7d5b56bcc76576bf42c912828c510aa8b
Justin <justinquach11214@gmail.com>
2023-07-20 15:08:08 -0400

    set up packages

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Coderace
+# Coderacer
+
+Test your typing skills
+
+
+reactJS
+node.js
+socket.io
+express
+mongoDB
+
+packages installed;
+express
+mongoose
+socket.io
+
---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io');
+const mongoose = require('mongoose');
+
+//3000 vs 3001; create react app by default listens to 3000; so pick unused port
+const expressServer = app.listen(3001);
+
+//returns http object so we pass that http to our socket io server
+const io = socketio(expressServer);
+
+//connect to mongolDb database locally
+mongoose.connect('mongodb://localhost:27017/coderacer', 
+                {useNewUrlParser: true, useUnifiedTopology : true},
+                ()=> { console.log('Successfully connected to database')});
